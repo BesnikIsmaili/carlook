4825b02609c379059ad6e07e9ac2f42550540be7
Besnik Ismaili <besnik.ismaili@smail.inf.h-brs.de>
2020-09-19 23:32:37 +0200

    implements util

---
+++ src/main/java/de/projekt/carlook/util/Attributes.java
@@ -0,0 +1,6 @@
+package de.projekt.carlook.util;
+
+public class Attributes {
+
+    public static final String USER = "user";
+}
---
+++ src/main/java/de/projekt/carlook/util/Views.java
@@ -0,0 +1,11 @@
+package de.projekt.carlook.util;
+
+public class Views {
+
+    public static final String LOGIN = "login";
+
+    public static final String REGISTRATION = "registration";
+
+    public static final String MAIN = "main";
+
+}
